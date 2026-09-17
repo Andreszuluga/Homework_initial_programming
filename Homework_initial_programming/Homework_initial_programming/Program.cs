a6a2d8caf0297590da71bc71b5118d4c58c92970
Andreszuluga <carlosandrescespedesz22@gmail.com>
2026-09-16 21:02:03 -0500

    Exercise one complete

---
+++ Homework_initial_programming/Homework_initial_programming/Program.cs
@@ -0,0 +1,17 @@
+﻿// Exercise one - Positive Power
+Console.Write("Ingrese un número: ");
+
+int numero = int.Parse(Console.ReadLine());
+
+if (numero > 0)
+{
+    Console.WriteLine("Resultado: " + (numero * numero));
+}
+else if (numero == 0)
+{
+    Console.WriteLine("Resultado: 0");
+}
+else
+{
+    Console.WriteLine("Número negativo");
+}
